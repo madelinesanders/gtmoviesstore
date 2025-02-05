be75b101c58fa0b4a3ba7198c81cd8689828f285
madelinesanders <63517681+madelinesanders@users.noreply.github.com>
2025-02-05 18:00:20 -0500

    Added home page and updated urls/settings in gtmoviesstore

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    # Placeholder movies
+    movies = [
+        {"id": 1, "title": "The Shawshank Redemption", "image_url": "/static/home/shawshank.jpg", "rating": 9.3},
+        {"id": 2, "title": "The Godfather", "image_url": "/static/home/thegodfather.jpg", "rating": 9.0},
+        {"id": 3, "title": "Schindler's List", "image_url": "/static/home/schindler.jpg", "rating": 9.0},
+    ]
+    return render(request, 'home/index.html', {"movies": movies})
