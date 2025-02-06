6a5697414575d0ac4ad30f32ae0f856bdcf65518
madelinesanders <63517681+madelinesanders@users.noreply.github.com>
2025-02-05 23:49:34 -0500

    Updated tye movie model, views, and migrations

--- home/views.py
+++ home/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
+from .models import Movie
 
 def index(request):
-    # Placeholder movies
-    movies = [
-        {"id": 1, "title": "The Shawshank Redemption", "image_url": "/static/home/shawshank.jpg", "rating": 9.3},
-        {"id": 2, "title": "The Godfather", "image_url": "/static/home/thegodfather.jpg", "rating": 9.0},
-        {"id": 3, "title": "Schindler's List", "image_url": "/static/home/schindler.jpg", "rating": 9.0},
-    ]
+    movies = Movie.objects.all()
     return render(request, 'home/index.html', {"movies": movies})
