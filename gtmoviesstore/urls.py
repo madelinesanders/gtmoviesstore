844e992e4ec6d7db187038da15cd861f9c67ceb4
madelinesanders <63517681+madelinesanders@users.noreply.github.com>
2025-02-06 00:03:11 -0500

    Added shopping cart app with URL routing

--- gtmoviesstore/urls.py
+++ gtmoviesstore/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("home/", include("home.urls"))
+    path("home/", include("home.urls")),
+    path('cart/', include('shopping_cart.urls')),
     #    path("admin/", admin.site.urls),
 ]
