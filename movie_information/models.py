05365addc5e453f41ada3c598cbfa325cebd7aca
Ayuri Srivastava <ayurisrivastava@lawn-128-61-50-221.lawn.gatech.edu>
2025-02-06 17:24:59 -0500

    made a git ignore file

--- movie_information/models.py
+++ movie_information/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User #check to see if this updates w team
+from home.models import Movie  #check to see if this updates w madeline
 
-# Create your models here.
+class Review(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="reviews")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  #foreignkey to user
+    rating = models.IntegerField(choices=[(i, i) for i in range(1, 6)])  #rating option
+    comment = models.TextField()  #review left by user
+    created_at = models.DateTimeField(auto_now_add=True)  #time review was made
+
+    def __str__(self):
+        return f"Review by {self.user.username} for {self.movie.title}"
