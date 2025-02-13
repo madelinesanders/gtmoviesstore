f86133198e8c41fa9025b6eb03b2e8abe62c58a3
madelinesanders <63517681+madelinesanders@users.noreply.github.com>
2025-02-12 20:25:25 -0500

    UI for shopping cart page

--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -15,7 +15,7 @@ def index(request):
     template_data['title'] = 'Cart'
     template_data['movies_in_cart'] = movies_in_cart
     template_data['cart_total'] = cart_total
-    return render(request, 'cart/index.html',
+    return render(request, 'shopping_cart/cart.html',
         {'template_data': template_data})
 
 def add(request, id):
