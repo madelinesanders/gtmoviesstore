6f9b221f152888fc70bb6839d2090dba09436a89
Chris <changhoon013@gmail.com>
2025-02-16 15:16:23 -0500

    orders

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,14 @@ from .forms import CustomUserCreationForm, CustomErrorList
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 @login_required
+def orders(request):
+    template_data = {}
+    template_data['title'] = 'Orders'
+    template_data['orders'] = request.user.order_set.all()
+    return render(request, 'accounts/orders.html',
+        {'template_data': template_data})
 def logout(request):
     auth_logout(request)
     return redirect('home.index')
