005e7036a3aa8504b326e84a7d104c380ee4514f
Huh, Christopher Changhoon <chuh7@gatech.edu>
2025-02-17 23:37:26 -0500

    debugged version

--- accounts/views.py
+++ accounts/views.py
@@ -1,54 +1,53 @@
-from django.shortcuts import render
-from django.contrib.auth import login as auth_login, authenticate
-from .forms import CustomUserCreationForm, CustomErrorList
-from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate
 from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm, CustomErrorList
 from django.contrib.auth.models import User
+
 @login_required
 def orders(request):
-    template_data = {}
-    template_data['title'] = 'Orders'
-    template_data['orders'] = request.user.order_set.all()
-    return render(request, 'accounts/orders.html',
-        {'template_data': template_data})
+    template_data = {
+        'title': 'Orders',
+        'orders': request.user.order_set.all()
+    }
+    return render(request, 'accounts/orders.html', template_data)
+
 def logout(request):
     auth_logout(request)
     return redirect('home.index')
+
 def login(request):
-    template_data = {}
-    template_data['title'] = 'Login'
+    template_data = {'title': 'Login'}
+
     if request.method == 'GET':
-        return render(request, 'accounts/login.html',
-            {'template_data': template_data})
+        return render(request, 'accounts/login.html', template_data)
+
     elif request.method == 'POST':
-        user = authenticate(
-            request,
-            username = request.POST['username'],
-            password = request.POST['password']
-        )
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+
+        user = authenticate(request, username=username, password=password)
+
         if user is None:
-            template_data['error'] =
-                'The username or password is incorrect.'
-            return render(request, 'accounts/login.html',
-                {'template_data': template_data})
+            template_data['error'] = 'The username or password is incorrect.'
+            return render(request, 'accounts/login.html', template_data)
         else:
             auth_login(request, user)
-            return redirect('accounts.login')
+            return redirect('home.index')  # Fixed redirect target
+
 def signup(request):
-    template_data = {}
-    template_data['title'] = 'Sign Up'
+    template_data = {'title': 'Sign Up'}
+
     if request.method == 'GET':
         template_data['form'] = CustomUserCreationForm()
-        return render(request, 'accounts/signup.html',
-            {'template_data': template_data})
+        return render(request, 'accounts/signup.html', template_data)
+
     elif request.method == 'POST':
-        form = CustomUserCreationForm(request.POST),
-        error_class = CustomErrorList)
+        form = CustomUserCreationForm(request.POST, error_class=CustomErrorList)  # Fixed misplaced parenthesis
+
         if form.is_valid():
             form.save()
             return redirect('home.index')
         else:
             template_data['form'] = form
-            return render(request, 'accounts/signup.html',
-                          {'template_data': template_data})
+            return render(request, 'accounts/signup.html', template_data)
